ada57d7e8b95932d4f528d0beff50ce8c7a9bfc1
Birb <cactushamsters@gmail.com>
2021-11-26 15:58:35 -0700

    Add powder preview to scripter.js

--- scripter.js
+++ scripter.js
@@ -1,11 +1,6 @@
 const gpu = new GPU();
 gpu.addFunction(prop)
-
-let newcanva = document.createElement('CANVAS')
-let genctx = newcanva.getContext('2d')
-
-
-
+let preview = true
 
 let tptcolors = ["0", "16769184", "2109648", "4210704", "16715776", "10526880", "14700560", "12632272", "2154512", "16764944", "14745376", "13664480", "8404992", "10535167", "4210784", "16777088", "12640511", "12623936", "2154751", "4223008", "830464", "15554047", "7932683", "10526975", "12632256", "1056960", "16777215", "4215024", "13434879", "5263472", "7360608", "16777215", "7368736", "15790267", "14737578", "8409168", "5263488", "8429791", "10396598", "3158064", "15724527", "13421772", "11184810", "5263424", "16765072", "4210752", "4214864", "6316128", "16777120", "12294655", "4210752", "12640511", "15419671", "8750341", "5263440", "16769184", "1063697", "2236962", "4456448", "2236962", "8429807", "8429823", "5521729", "15655104", "12300448", "10518672", "4478052", "8421504", "8421631", "16752704", "677638", "6532447", "8421504", "3355443", "3881738", "3869194", "7368816", "14737632", "830464", "7681424", "6710886", "14737632", "2109648", "5300191", "7938080", "699146", "8454048", "16777152", "7368768", "16724753", "16760368", "13421772", "11184810", "16764992", "16724223", "5439", "1052688", "657979", "6710886", "4473924", "12640511", "1056960", "2105376", "65399", "65467", "58453", "4210745", "11153616", "6693001", "15423767", "8427", "11180458", "7825015", "16777198", "498176", "52428", "16769184", "11184810", "11184810", "11184810", "7829367", "4473924", "2171169", "5673490", "4235360", "86", "16759552", "16777215", "6579455", "16773768", "2117856", "2368548", "11197917", "8960955", "6728447", "7368800", "14675967", "39372", "10079232", "12603472", "12628048", "668160", "16750848", "11140864", "9474192", "2391038", "00000000", "16773768", "5271807", "16119260", "2105376", "16777215", "7564141", "3874058", "670523", "14984356", "16763904", "1131707", "16769184", "48127", "10079232", "4473958", "16620824", "8978312", "16580821", "20480", "20480", "12320512", "11180441", "10066312", "14462252", "5263440", "14360608", "10027008", "16650742", "13895885", "16673022", "61046", "16755234", "14078420", "13329233", "15790240", "5267504", "7525076", "8700623", "3368601", "6750054", "12373471", "14016747", "8166400", "7500402", "11971263"]
 //let avgclr = []
@@ -20,7 +15,7 @@ function getColor(v) {
 
 //new code stuff
 
-let colors = tptcolors.map((val) => {
+let elcolors = tptcolors.map((val) => {
 	return getColor(val);
 });
 
@@ -32,7 +27,7 @@ function getClosest(r, g, b) {
 	//returns the index of closest elem to rgb value
 	let minDist = Infinity;
 	let minI = 0;
-	colors.forEach((item, i) => {
+	elcolors.forEach((item, i) => {
 		let d = dist(...item, r, g, b);
 		if (d < minDist) {
 			minI = i;
@@ -75,6 +70,10 @@ function generateScript(deco, elems, useFill, useAlpha, fillBlack) {
 
 	genimage.src = canvas.toDataURL()
 	genimage.onload = () => {
+		if (preview && elems) {
+			ctx.fillStyle = 'black'
+			ctx.fillRect((canvas.width / 2) - ((simSize.w / 2)), ((canvas.height / 2) - (simSize.h / 2)), simSize.w, simSize.h)
+		}
 		//let script = ['']
 		let functionName = functionnameInput.value.toString().trim()
 		if (functionName == '') functionName = 'image'
@@ -101,16 +100,21 @@ function generateScript(deco, elems, useFill, useAlpha, fillBlack) {
 						let u = getClosest(...c) //Sets u to the closest colored tpt element index
 						matches[j] = u
 					}
-					c = matches[j] ?? c
-					if (c == 4) c = 89 //Fire bad, dest good
+					c = matches[j]
+					if (preview) {
+						ctx.fillStyle = 'rgb('+elcolors[c].join(',')+')'
+						ctx.fillRect(+x+xf, canvas.height-(+y+yf), 1, 1)
+					}
 				}
 				else {
 					c = defaultElem
 				}
+				//Fire bad, dest good
+				if (c == 4) c = 89
 				//LCRY to BLGA
-				if (c == 54) c = 47
+				else if (c == 54) c = 47
 				//Set GPMP life to 0
-				if (c == 154) script.push(`a = sim.partCreate(-3, ${x}, ${offsettedY}, ${c})\nsim.partProperty(a, sim.FIELD_LIFE, 0)`)
+				else if (c == 154) script.push(`a = sim.partCreate(-3, ${x}, ${offsettedY}, ${c})\nsim.partProperty(a, sim.FIELD_LIFE, 0)`)
 				//Set CLST tmp to 5
 				else if (c == 155) script.push(`a = sim.partCreate(-3, ${x}, ${offsettedY}, ${c})\nsim.partProperty(a, sim.FIELD_TMP, 5)`)
 				//Change TRON to lime green-ish
@@ -136,6 +140,6 @@ function generateScript(deco, elems, useFill, useAlpha, fillBlack) {
 		if (title == '.lua') title = ((imageInput.files[0] ?? {}).name ?? 'img')
 		if (!title.endsWith('.lua')) title = title + '.lua'
 		download(title, script.join('\n'))
-		render()
+		if (!preview || !elems) render()
 	};
 }
